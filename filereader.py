9b5c51b9cefb3dc2088e4a11f9b6b00a651a2718
srvsngh200892 <saurav.kumar@hashedin.com>
2014-07-08 13:55:57 +0530

    Delete filereader.py

--- filereader.py
+++
@@ -1,9 +0,0 @@
-rating_array = []
-with open("/home/hasher/Downloads/movie_recommendation/ratings.data", "r") as f:
-     for line in f:
-         rating_array.append(line)
-rating_list=[]
-for i in rating_array:
-    x=i.split('\t')
-    rating_list.append(x)
-print rating_list
